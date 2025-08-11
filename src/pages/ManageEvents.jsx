de972047b4d7903d4c7e2a773b5ea6aee81ce346
Syed1708 <snahmed2017@gmail.com>
2025-08-12 00:45:01 +0200

    add pagination manage events

--- src/pages/ManageEvents.jsx
+++ src/pages/ManageEvents.jsx
@@ -12,6 +12,11 @@ const ManageEvents = () => {
   const { myEventsPromise, deleteEvent } = useEventsApi();
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
+  
+  const ITEMS_PER_PAGE = 3;
+  const [currentPage, setCurrentPage] = useState(1);
+
+
 
   useEffect(() => {
     myEventsPromise(user.email)
@@ -49,19 +54,57 @@ const ManageEvents = () => {
     }
   };
 
+
+      // Pagination
+  const totalPages = Math.ceil(events.length / ITEMS_PER_PAGE); 
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const currentEvents = events.slice(
+    startIndex,
+    startIndex + ITEMS_PER_PAGE
+  );
+
+  const goPrev = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
+  const goNext = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
+
   if (loading) return <Loading2 />;
 
   return (
     <div className="max-w-6xl mx-auto py-10 px-4">
       <h2 className="text-3xl font-bold mb-6 text-center">Manage My Events</h2>
-      {events.length > 0 ? (
+      {currentEvents.length > 0 ? (
         <>
           <Suspense>
             <EventsList
-              events={events}
+              currentEvents={currentEvents}
               handleDelete={handleDelete}
             ></EventsList>
           </Suspense>
+
+                    {/* Pagination */}
+          <div className="flex justify-center items-center gap-4 mt-6">
+            <button
+              onClick={goPrev}
+              disabled={currentPage === 1}
+              className="btn btn-sm btn-outline"
+            >
+              Previous
+            </button>
+            <span>
+              Page {currentPage} of {totalPages}
+            </span>
+            <button
+              onClick={goNext}
+              disabled={currentPage === totalPages}
+              className="btn btn-sm btn-outline"
+            >
+              Next
+            </button>
+          </div>
         </>
       ) : (
         <div>
